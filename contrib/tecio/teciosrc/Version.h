647080b88e22217f9b58f0ace2cfcc26359b1015
David Andrs <andrsd@gmail.com>
2025-05-19 07:17:32 +0200

    Adding TecIO source from TecPlot, Inc.

---
+++ contrib/tecio/teciosrc/Version.h
@@ -0,0 +1,9 @@
+ #pragma once
+ #define ___4026 "Tecio Care Package"
+ #define TecplotMajorVersion 2024
+ #define TecplotMinorVersion 1
+ #define TecplotPatchVersion 0
+ #define TecplotRepositoryRevision 51129
+ #define TecplotProductVersion "2024.1.0.51129"
+ #define TecplotProductEdition ""
+ #define TecplotProductRelease ""
