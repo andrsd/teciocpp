7999484c277e1502a763a0e724db85818abe6449
David Andrs <andrsd@gmail.com>
2025-07-15 06:04:43 -0600

    Use gcov we discovered via find_program

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -79,6 +79,10 @@ if (TECIOCPP_CODE_COVERAGE)
 
     elseif (CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
         find_program(GCOV_PATH NAMES gcov)
+        if (NOT GCOV_PATH)
+            message(FATAL_ERROR "gcov not found!")
+        endif()
+
         find_program(LCOV_PATH lcov)
         find_program(GENHTML_PATH genhtml)
         mark_as_advanced(FORCE
@@ -105,6 +109,7 @@ if (TECIOCPP_CODE_COVERAGE)
                 --capture
                 --directory ${PROJECT_BINARY_DIR}
                 --output-file ${COVERAGE_INFO}
+                --gcov-tool ${GCOV_PATH}
                 ${EXCLUDE_REGEX}
         )
 
